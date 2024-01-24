8328abf0dd49ee9a95420a5ec874b38272357772
William Tucker <william.tucker29@gmail.com>
2024-01-24 14:10:58 -0600

    Passed functional pattern for queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+
+    //size--;
+    // remove the first item added
+    var keys = Object.keys(storage);
+    var result = storage[keys[0]];
+    delete storage[keys[0]];
+    return result;
+
   };
 
   someInstance.size = function() {
+    if (size <= 0) {
+      return 0;
+    }
+    return Object.keys(storage).length;
   };
 
   return someInstance;
