7e7b1d9ab4d785d5605fce4a11c8515fa9ef034b
William Tucker <william.tucker29@gmail.com>
2024-01-24 14:11:37 -0600

    Pass functional test for stacks

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = (value);
+    size++;
   };
 
   someInstance.pop = function() {
+
+    size--;
+    var result = storage[size];
+    delete storage[size];
+    return result;
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      return 0;
+    }
+    return size;
   };
 
   return someInstance;
