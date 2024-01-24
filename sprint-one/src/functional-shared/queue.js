b9c5d2fd89323d1c262899dfb903ea9980b27695
William Tucker <william.tucker29@gmail.com>
2024-01-24 17:22:40 -0600

    Pass all tests for queue and stack func-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {
+    index: 0,
+    enqueueCount: 0,
+    dequeueCount: 0
+  };
+  var size = 0;
+  _.extend(storage, queueMethods);
+  return storage;
 };
 
 var queueMethods = {};
 
 
+queueMethods.enqueue = function(value) {
+  this[this.index] = value;
+  this.index++;
+  this.enqueueCount++;
+};
+
+queueMethods.dequeue = function() {
+  var keys = Object.keys(this);
+  var result = this[keys[0]];
+  delete this[keys[0]];
+  this.dequeueCount++;
+  return result;
+};
+
+queueMethods.size = function() {
+  if (this.enqueueCount - this.dequeueCount <= 0) {
+    return 0;
+  }
+  return this.enqueueCount - this.dequeueCount;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {
+    index: 0
+  };
+
+  _.extend(storage, stackMethods);
+  return storage;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this[this.index] = value;
+  this.index++;
+};
+
+stackMethods.pop = function() {
+  this.index--;
+  var result = this[this.index];
+  delete this[this.index];
+  return result;
+};
+
+stackMethods.size = function() {
+  if (this.index < 0) {
+    return 0;
+  }
+  return this.index;
+};
 
