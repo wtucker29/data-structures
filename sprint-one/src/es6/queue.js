36b1f119ea9d3f42e4e60ba0eb076aee3af4e982
William Tucker <william.tucker29@gmail.com>
2024-01-24 18:06:57 -0600

    Pass tests for es6 functions

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.index = 0;
+    this.enqueueCount = 0;
+    this.dequeueCount = 0;
+  }
+
+  enqueue(value) {
+    this[this.index] = value;
+    this.index++;
+    this.enqueueCount++;
+  }
+
+  dequeue() {
+    this.dequeueCount++;
+    var keys = Object.keys(this);
+    var result = this[keys[0]];
+    delete this[keys[0]];
+    return result;
+  }
+
+  size() {
+    if (this.enqueueCount - this.dequeueCount <= 0) {
+      return 0;
+    }
+    return this.enqueueCount - this.dequeueCount;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.index = 0;
+  }
+
+  push(value) {
+    this[this.index] = value;
+    this.index++;
+  }
+
+  pop() {
+    this.index--;
+    var result = this[this.index];
+    delete this[this.index];
+    return result;
+  }
+
+  size() {
+    if (this.index <= 0) {
+      return 0;
+    }
+    return this.index;
   }
 
 }
