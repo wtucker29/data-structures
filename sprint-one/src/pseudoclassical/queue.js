70e938160b675d53d601664991ccc74d90a5c706
William Tucker <william.tucker29@gmail.com>
2024-01-24 18:00:15 -0600

    Pass tests for psuedoclassical functions

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.index = 0;
+  this.enqueueCount = 0;
+  this.dequeueCount = 0;
 };
 
 
+Queue.prototype.enqueue = function(value) {
+  this[this.index] = value;
+  this.index++;
+  this.enqueueCount++;
+};
+
+Queue.prototype.dequeue = function() {
+  this.dequeueCount++;
+  var keys = Object.keys(this);
+  var result = this[keys[0]];
+  delete this[keys[0]];
+  return result;
+};
+
+Queue.prototype.size = function() {
+  if (this.enqueueCount - this.dequeueCount <= 0) {
+    return 0;
+  }
+  return this.enqueueCount - this.dequeueCount;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.index = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this[this.index] = value;
+  this.index++;
+};
 
+Stack.prototype.pop = function() {
+  this.index--;
+  var result = this[this.index];
+  delete this[this.index];
+  return result;
+};
+
+Stack.prototype.size = function() {
+  if (this.index <= 0) {
+    return 0;
+  }
+  return this.index;
+};
